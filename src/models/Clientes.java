10fb5a8a4f8748283a166fabbf1591bced31cc02
mel091 <melc53774@gmail.com>
2024-05-28 00:50:50 -0700

    logica de crear cliente y getters

---
+++ src/models/Clientes.java
@@ -0,0 +1,77 @@
+package models;
+
+public class Clientes 
+{
+	private String idCliente;
+	private String nombreCompleto;
+	private String correo;
+	private String telefono;
+	private String direccion;
+	private String contactoEmergencia;
+	private String relacionCliente;
+	private String telefonoEmergencia;
+	private String infAdicional;
+	private String estatus;
+	private byte[] imagen;
+	
+	public Clientes(String idCliente, String nombreCompleto, String correo, String telefono, String direccion,
+			String contactoEmergencia, String relacionCliente, String telefonoEmergencia, String infAdicional, String estatus,  byte[] imagen)
+	{
+		this.idCliente = idCliente;
+		this.nombreCompleto = nombreCompleto;
+		this.correo = correo;
+		this.telefono = telefono;
+		this.direccion = direccion;
+		this.contactoEmergencia = contactoEmergencia;
+		this.relacionCliente = relacionCliente;
+		this.telefonoEmergencia = telefonoEmergencia;
+		this.infAdicional = infAdicional;
+		this.estatus = estatus;
+		this.imagen = imagen;
+	}
+
+	public String getIdCliente() {
+		return idCliente;
+	}
+
+	public String getNombreCompleto() {
+		return nombreCompleto;
+	}
+
+	public String getCorreo() {
+		return correo;
+	}
+
+	public String getTelefono() {
+		return telefono;
+	}
+
+	public String getDireccion() {
+		return direccion;
+	}
+
+	public String getContactoEmergencia() {
+		return contactoEmergencia;
+	}
+
+	public String getRelacionCliente() {
+		return relacionCliente;
+	}
+
+	public String getTelefonoEmergencia() {
+		return telefonoEmergencia;
+	}
+
+	public String getInfAdicional() {
+		return infAdicional;
+	}
+
+	public String getEstatus() {
+		return estatus;
+	}
+
+	public byte[] getImagen() {
+		return imagen;
+	}
+
+}
--- src/models/ClientesModel.java
+++ src/models/ClientesModel.java
@@ -1,33 +1,135 @@
 package models;
 
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+
+import javax.imageio.ImageIO;
+import javax.swing.ImageIcon;
+import javax.swing.JButton;
+import javax.swing.JFileChooser;
+import javax.swing.JTextArea;
+import javax.swing.JTextField;
+import javax.swing.filechooser.FileNameExtensionFilter;
+import javax.swing.table.DefaultTableModel;
+
 import controllers.ClientesController;
 import controllers.HabitacionesController;
+import views.ClientesView;
 
 public class ClientesModel 
 {
-	public ClientesController cliente;
+	JFileChooser fc;
+	private InputStream imagenSeleccionada;
+	ClientesView view;
+	private JTextField idC;
+	private JTextField nombre;
+	private JTextField correo;
+	private JTextField telefono;
+	private JTextField direccion;
+	private JTextField nombreEmergencia;
+	private JTextField relacion;
+	private JTextField telefonoEmergencia;
+	private JTextArea infoAdicional;
+	private JTextField estatus;
+	
 	
 	public ClientesModel()
 	{
 		
 	}
 	
+	public void crear(String nombreC, String correo, String tel, String dir, String contactoEmergencia, 
+			String relacion, String telEmergencia, String inf, InputStream img)
+	{
+		try 
+		{
+			Connection cn = Conexion.conectar();
+			PreparedStatement pst = cn.prepareStatement("insert into clientes (idCliente, nombreCompleto, correo, telefono, direccion, contactoEmergencia, relacionCliente,"
+					+ "telefonoEmergencia, infAdicional, img) values(?, ?, ?, ?, ?, ?, ?, ?, ?, ?)"); //signos = num columnas
+			
+			pst.setString(1, "0");
+			pst.setString(2, nombreC.trim()); //trim quita espacios del inicio y final del input
+			pst.setString(3, correo);
+			pst.setString(4, tel);
+			pst.setString(5, dir);
+			pst.setString(6, contactoEmergencia);
+			pst.setString(7, relacion);
+			pst.setString(8, telEmergencia);
+			pst.setString(9, inf);
+			
+			if(img != null)
+			{
+				//pst.setBlob(10, img);
+				pst.setBinaryStream(10, img);
+			}
+			else
+			{
+				pst.setNull(10, java.sql.Types.BLOB);
+			}
+			
+			pst.executeUpdate(); // EJECUTAR INSTRUCCIONES ENVIADAS A LA BASE DE DATOS, PONER PARA QUE FUNCIONE.
+			
+			System.out.println("registro exitoso");
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
 	public void descargar()
 	{
-		cliente = new ClientesController();
-		cliente.descarga();
+		
 	}
 	
 	public void historial()
 	{
-		cliente = new ClientesController();
-		cliente.historial();
+		
 	}
 	
 	public void subirImg() //preguntar
 	{
-		//proceso
-		System.out.println("sube imagen");
+		fc = new JFileChooser();
+		FileNameExtensionFilter filter = new FileNameExtensionFilter("jpg", "png", "jpeg");
+		fc.setFileFilter(filter);
+		int r = fc.showOpenDialog(null);
+		
+		if(r == JFileChooser.APPROVE_OPTION)
+		{
+			try {
+				File selectFile = fc.getSelectedFile();
+				imagenSeleccionada = new FileInputStream(selectFile);
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+			
+		}
+	}
+	
+	public void imagen(JButton btn) //mostrar imagen encima del btn
+	{ 
+		if (imagenSeleccionada != null) {
+            try {
+                BufferedImage img = ImageIO.read(imagenSeleccionada);
+                ImageIcon icon = new ImageIcon(img);
+                btn.setIcon(icon);
+            } catch (IOException ex) {
+                ex.printStackTrace();
+            }
+        } else {
+        	btn.setIcon(new ImageIcon(getClass().getResource("/contenido/subirCliente.png")));
+        }
+	}
+	
+	
+	
+	public InputStream getImagen()
+	{
+		return imagenSeleccionada;
 	}
 
 }
